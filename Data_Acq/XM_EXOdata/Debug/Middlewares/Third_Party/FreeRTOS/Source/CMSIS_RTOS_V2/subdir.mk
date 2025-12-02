37a3f539d5d992dc9796dbb80ec12174778a1ca2
kkyle0927 <kkyle0927@kaist.ac.kr>
2025-12-02 14:47:08 +0900

    Base

---
+++ Data_Acq/XM_EXOdata/Debug/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c 
+
+OBJS += \
+./Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.o 
+
+C_DEPS += \
+./Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/%.o Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/%.su Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/%.cyclo: ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/%.c Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu18 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32H743xx -DUSE_PWR_LDO_SUPPLY -c -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_Apps/User_Algorithm" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/XM_API" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Board/Button" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Board/GPIO" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Board/LED" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Comm/CANFD" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Comm/UART" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Comm/USB" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Config" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Core" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Links/Control_Module" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Links/GRF_Module" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Links/IMU_Module" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Links/PnP_Manager" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Devices/Marveldex" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Devices/XSENS" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Devices/AGR/Control_Module" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Services/Buff_Mngr/Ring_Buffer/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Services/Task_State_Machine/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Services/DOP_Mngr/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Services/Risk_Mngr/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/IOIF/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/Drivers/STM32H7xx_HAL_Driver/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/FATFS/Target" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/STM32_USB_Device_Library/Class/CDC/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/STM32_USB_Device_Library/Core/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/STM32_USB_Host_Library/Class/MSC/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/STM32_USB_Host_Library/Core/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/USB_DEVICE/App" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/USB_DEVICE/Target" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/USB_HOST/App" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/USB_HOST/Target" -I../Core/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../Drivers/CMSIS/Include -I../FATFS/Target -I../FATFS/App -I../Middlewares/Third_Party/FatFs/src -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Middlewares-2f-Third_Party-2f-FreeRTOS-2f-Source-2f-CMSIS_RTOS_V2
+
+clean-Middlewares-2f-Third_Party-2f-FreeRTOS-2f-Source-2f-CMSIS_RTOS_V2:
+	-$(RM) ./Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.cyclo ./Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.d ./Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.o ./Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.su
+
+.PHONY: clean-Middlewares-2f-Third_Party-2f-FreeRTOS-2f-Source-2f-CMSIS_RTOS_V2
+
---
+++ Data_Acq/XM_EXOdata/Debug/XM_FW/Services/DOP_Mngr/Src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../XM_FW/Services/DOP_Mngr/Src/data_object.c \
+../XM_FW/Services/DOP_Mngr/Src/data_object_common.c \
+../XM_FW/Services/DOP_Mngr/Src/data_object_dictionaries.c \
+../XM_FW/Services/DOP_Mngr/Src/data_object_interface.c 
+
+OBJS += \
+./XM_FW/Services/DOP_Mngr/Src/data_object.o \
+./XM_FW/Services/DOP_Mngr/Src/data_object_common.o \
+./XM_FW/Services/DOP_Mngr/Src/data_object_dictionaries.o \
+./XM_FW/Services/DOP_Mngr/Src/data_object_interface.o 
+
+C_DEPS += \
+./XM_FW/Services/DOP_Mngr/Src/data_object.d \
+./XM_FW/Services/DOP_Mngr/Src/data_object_common.d \
+./XM_FW/Services/DOP_Mngr/Src/data_object_dictionaries.d \
+./XM_FW/Services/DOP_Mngr/Src/data_object_interface.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+XM_FW/Services/DOP_Mngr/Src/%.o XM_FW/Services/DOP_Mngr/Src/%.su XM_FW/Services/DOP_Mngr/Src/%.cyclo: ../XM_FW/Services/DOP_Mngr/Src/%.c XM_FW/Services/DOP_Mngr/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu18 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32H743xx -DUSE_PWR_LDO_SUPPLY -c -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_Apps/User_Algorithm" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/XM_API" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Board/Button" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Board/GPIO" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Board/LED" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Comm/CANFD" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Comm/UART" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Comm/USB" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Config" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Core" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Links/Control_Module" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Links/GRF_Module" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Links/IMU_Module" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/System/Links/PnP_Manager" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Devices/Marveldex" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Devices/XSENS" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Devices/AGR/Control_Module" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Services/Buff_Mngr/Ring_Buffer/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Services/Task_State_Machine/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Services/DOP_Mngr/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/Services/Risk_Mngr/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/XM_FW/IOIF/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/Drivers/STM32H7xx_HAL_Driver/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/FATFS/Target" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/STM32_USB_Device_Library/Class/CDC/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/STM32_USB_Device_Library/Core/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/STM32_USB_Host_Library/Class/MSC/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/STM32_USB_Host_Library/Core/Inc" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/USB_DEVICE/App" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/USB_DEVICE/Target" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/USB_HOST/App" -I"C:/Github/IIT/Data_Acq/XM_EXOdata/Compatible/USB_HOST/Target" -I../Core/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../Drivers/CMSIS/Include -I../FATFS/Target -I../FATFS/App -I../Middlewares/Third_Party/FatFs/src -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-XM_FW-2f-Services-2f-DOP_Mngr-2f-Src
+
+clean-XM_FW-2f-Services-2f-DOP_Mngr-2f-Src:
+	-$(RM) ./XM_FW/Services/DOP_Mngr/Src/data_object.cyclo ./XM_FW/Services/DOP_Mngr/Src/data_object.d ./XM_FW/Services/DOP_Mngr/Src/data_object.o ./XM_FW/Services/DOP_Mngr/Src/data_object.su ./XM_FW/Services/DOP_Mngr/Src/data_object_common.cyclo ./XM_FW/Services/DOP_Mngr/Src/data_object_common.d ./XM_FW/Services/DOP_Mngr/Src/data_object_common.o ./XM_FW/Services/DOP_Mngr/Src/data_object_common.su ./XM_FW/Services/DOP_Mngr/Src/data_object_dictionaries.cyclo ./XM_FW/Services/DOP_Mngr/Src/data_object_dictionaries.d ./XM_FW/Services/DOP_Mngr/Src/data_object_dictionaries.o ./XM_FW/Services/DOP_Mngr/Src/data_object_dictionaries.su ./XM_FW/Services/DOP_Mngr/Src/data_object_interface.cyclo ./XM_FW/Services/DOP_Mngr/Src/data_object_interface.d ./XM_FW/Services/DOP_Mngr/Src/data_object_interface.o ./XM_FW/Services/DOP_Mngr/Src/data_object_interface.su
+
+.PHONY: clean-XM_FW-2f-Services-2f-DOP_Mngr-2f-Src
+
